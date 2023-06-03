1f6cfbd6b18298910faa5166dd7904831dd369dc
Danila <icy.calmness@gmail.com>
2023-06-03 17:08:18 +0300

    add config.py

---
+++ Module2/homework_6/config.py
@@ -0,0 +1,10 @@
+WORDS_PATH = "words.txt"
+HISTORY_PATH = "history.txt"
+
+
+with open(WORDS_PATH, "wt", encoding="utf-8") as file:
+    file.write("python\n")
+    file.write("squirrel\n")
+    file.write("rabbit\n")
+    file.write("elephant\n")
+    file.write("falcon\n")
